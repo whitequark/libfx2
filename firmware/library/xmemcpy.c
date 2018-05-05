8448a90c612b9c8bb4462c385aaf1ac7e6811792
whitequark <whitequark@whitequark.org>
2018-05-05 17:09:34 +0000

    Add medium, large and huge memory model support to xmemcpy.
    Wow, this is a pain.

--- firmware/library/xmemcpy.c
+++ firmware/library/xmemcpy.c
@@ -1,6 +1,30 @@
 #include <fx2lib.h>
 #include <fx2regs.h>
 
+#if defined(__SDCC_MODEL_SMALL)
+#define GET_PARM(rA, rB, parm) \
+    mov  rA, parm+0 \
+    mov  rB, parm+1
+#elif defined(__SDCC_MODEL_MEDIUM)
+#define HASH #
+#define GET_PARM(rA, rB, parm) \
+    mov  r0, HASH parm \
+    movx a, @r0 \
+    mov  rA, a \
+    inc  r0 \
+    movx a, @r0 \
+    mov  rB, a
+#elif defined(__SDCC_MODEL_LARGE) || defined(__SDCC_MODEL_HUGE)
+#define HASH #
+#define GET_PARM(rA, rB, parm) \
+    mov  dptr, HASH parm \
+    movx a, @dptr \
+    mov  rA, a \
+    inc  dptr \
+    movx a, @dptr \
+    mov  rB, a
+#endif
+
 __xdata void *xmemcpy(__xdata void *dest, __xdata void *src, uint16_t length) {
   dest;
   src;
@@ -11,11 +35,9 @@ __xdata void *xmemcpy(__xdata void *dest, __xdata void *src, uint16_t length) {
     push _DPH1
     push _DPL1
     push _DPS
-    push ar6
-    push ar7
 
-    mov  r6, _xmemcpy_PARM_3+0
-    mov  r7, _xmemcpy_PARM_3+1
+    GET_PARM(r4, r5, _xmemcpy_PARM_2)
+    GET_PARM(r6, r7, _xmemcpy_PARM_3)
 
     // Handle edge conditions.
     // Skip the entire function if r7:r6=0.
@@ -33,8 +55,8 @@ __xdata void *xmemcpy(__xdata void *dest, __xdata void *src, uint16_t length) {
     mov  _AUTOPTRSETUP, #0b111 ; ATPTR2INC|APTR1INC|APTREN
     mov  _AUTOPTRL1, _DPL0
     mov  _AUTOPTRH1, _DPH0
-    mov  _AUTOPTRL2, _xmemcpy_PARM_2+0
-    mov  _AUTOPTRH2, _xmemcpy_PARM_2+1
+    mov  _AUTOPTRL2, r4
+    mov  _AUTOPTRH2, r5
 
     // Set up data pointers.
     mov  _DPS, #0
@@ -52,8 +74,6 @@ __xdata void *xmemcpy(__xdata void *dest, __xdata void *src, uint16_t length) {
       djnz r7, 00001$   ; 4c
 
   00002$:
-    pop  ar7
-    pop  ar6
     pop  _DPS
     pop  _DPL1
     pop  _DPH1
