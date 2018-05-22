2574bfd8c9ce2c02a64d6138a0a16fbf370f503a
whitequark <whitequark@whitequark.org>
2018-05-22 02:49:02 +0000

    Avoid generating junk on SCL after an I2C read.

--- firmware/library/i2c.c
+++ firmware/library/i2c.c
@@ -65,9 +65,29 @@ bool i2c_read(uint8_t *buf, uint16_t len) {
       goto end;
     if(i + 2 == len)
       I2CS = _LASTRD;
-    else if(i + 1 == len)
-      I2CS = _STOP;
-    buf[i] = I2DAT;
+    if(i + 1 == len) {
+      // TRM 13.5.4.14: Read the final byte from I2DAT immediately (the next
+      // instruction) after setting the STOP bit.
+      // This is quite tricky to actually achieve; we use the autopointers
+      // and the fact that I2DAT immediately follows I2CS.
+      // Alternatively, MPAGE could be used.
+      uint8_t tmp;
+__asm
+      mov  _AUTOPTRSETUP, #0b11 ; APTR1INC|APTREN
+      mov  _AUTOPTRH1, #(_I2CS >> 8)
+      mov  _AUTOPTRL1, #(_I2CS & 0xff)
+      mov  dptr, #_XAUTODAT1
+      mov  a, #0b01000000 ; STOP
+      movx @dptr, a
+      movx a, @dptr
+__endasm;
+      // This ensures that ACC won't get overwritten by the compiler before
+      // it has a chance to get saved to buf[i].
+      tmp = ACC;
+      buf[i] = tmp;
+    } else {
+      buf[i] = I2DAT;
+    }
   }
 
   while(I2CS & _STOP);
