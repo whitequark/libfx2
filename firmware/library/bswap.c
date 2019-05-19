65a89e210fce8f888e1b838a2604a025f13d9983
whitequark <whitequark@whitequark.org>
2019-05-19 19:32:59 +0000

    Fix bswap16 to return correct result.
    Also, use optimal exchange sequence for both bswap16 and bswap32.

--- firmware/library/bswap.c
+++ firmware/library/bswap.c
@@ -4,13 +4,13 @@
 uint16_t bswap16(uint16_t value) __naked {
   value;
 __asm;
-  mov  a, dph
-  mov  dpl, dph
-  mov  dpl, a
-#if defined(__SDCC_MODEL_HUGE)
-  ljmp  __sdcc_banked_ret
+  xch  a, dpl   ; 2c
+  xch  a, dph   ; 2c
+  xch  a, dpl   ; 2c
+#if !defined(__SDCC_MODEL_HUGE)
+  ret           ; 4c
 #else
-  ret
+  ljmp  __sdcc_banked_ret
 #endif
 __endasm;
 }
@@ -18,23 +18,14 @@ __endasm;
 uint32_t bswap32(uint32_t value) __naked {
   value;
 __asm;
-#if defined(__SDCC_MODEL_SMALL)
-  ar0 = 0x00
-  push ar0
-#endif
-  mov  r0, a
-  mov  a, dpl
-  mov  dpl, r0
-  mov  r0, b
-  mov  b, dph
-  mov  dph, r0
-#if defined(__SDCC_MODEL_SMALL)
-  pop  ar0
-#endif
-#if defined(__SDCC_MODEL_HUGE)
-  ljmp  __sdcc_banked_ret
+  xch  a, dpl   ; 2c
+  xch  a, b     ; 2c
+  xch  a, dph   ; 2c
+  xch  a, b     ; 2c
+#if !defined(__SDCC_MODEL_HUGE)
+  ret           ; 4c
 #else
-  ret
+  ljmp  __sdcc_banked_ret
 #endif
 __endasm;
 }
