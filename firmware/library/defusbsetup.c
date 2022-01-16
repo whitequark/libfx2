23bafa38d38fb17105662677b938bfabc39a9965
Catherine <whitequark@whitequark.org>
2022-01-16 14:29:58 +0000

    Fix typo in default handle_usb_setup() definition.
    Fixes #7.

--- firmware/library/defusbsetup.c
+++ firmware/library/defusbsetup.c
@@ -1,6 +1,6 @@
 #include <fx2usb.h>
 
-void handle_usb_request(__xdata struct usb_req_setup *request) {
+void handle_usb_setup(__xdata struct usb_req_setup *request) {
   request;
   STALL_EP0();
 }
