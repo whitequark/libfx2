e49d12e916d244841e70e760a0bfb603fd26f3b8
bifferos <bifferos@users.noreply.github.com>
2025-01-26 20:25:49 +0000

    Run against all clock speeds

--- examples/printf/main.c
+++ examples/printf/main.c
@@ -2,19 +2,28 @@
 #include <fx2debug.h>
 #include <stdio.h>
 
+
+// An example of how to push characters to a software UART on PA0.
+// As a test, the CLK speed is changed to check the code works for
+// a variety of frequencies.
+// 38400 likely works in all scenarios.  If you increase the baud
+// rate it may or may not work, but  
+
+
 DEFINE_DEBUG_PUTCHAR_FN(PA0, 38400)
 
 int main(void) {
-  // Any of these will work at 38400 baud:
-
   OEA = (1U<<0);
   PA0 = 1;
 
   CPUCS = 0;
   printf("Hello, world at 12MHz CPU clock!\r\n");
+  
   CPUCS = _CLKSPD0;
   printf("Hello, world at 24MHz CPU clock!\r\n");
+  
   CPUCS = _CLKSPD1;
   printf("Hello, world at 48MHz CPU clock!\r\n");
+  
   while(1);
 }
