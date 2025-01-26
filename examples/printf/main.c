4e7c64fb27008ff1eaff925b0d6e56bb35161459
bifferos <bifferos@users.noreply.github.com>
2025-01-26 20:11:53 +0000

    57600 -> 38400 baud change

--- examples/printf/main.c
+++ examples/printf/main.c
@@ -2,17 +2,19 @@
 #include <fx2debug.h>
 #include <stdio.h>
 
-DEFINE_DEBUG_PUTCHAR_FN(PA0, 57600)
+DEFINE_DEBUG_PUTCHAR_FN(PA0, 38400)
 
 int main(void) {
-  // Any of these will work at 57600 baud:
-  CPUCS = 0;
-  // CPUCS = _CLKSPD0;
-  // CPUCS = _CLKSPD1;
+  // Any of these will work at 38400 baud:
 
   OEA = (1U<<0);
   PA0 = 1;
 
-  printf("Hello, world!\n");
+  CPUCS = 0;
+  printf("Hello, world at 12MHz CPU clock!\r\n");
+  CPUCS = _CLKSPD0;
+  printf("Hello, world at 24MHz CPU clock!\r\n");
+  CPUCS = _CLKSPD1;
+  printf("Hello, world at 48MHz CPU clock!\r\n");
   while(1);
 }
