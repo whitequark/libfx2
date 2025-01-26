013416096fe4cae9502e54f7409b8523ab91a259
bifferos <bifferos@yahoo.co.uk>
2025-01-26 20:43:25 +0000

    Use 38400 baud instead of 57600 baud in `examples/printf`.
    Co-authored-by: bifferos <bifferos@users.noreply.github.com>

--- examples/printf/main.c
+++ examples/printf/main.c
@@ -2,17 +2,27 @@
 #include <fx2debug.h>
 #include <stdio.h>
 
-DEFINE_DEBUG_PUTCHAR_FN(PA0, 57600)
+
+// An example of how to push characters to a software UART on PA0.
+// As a test, the CLK speed is changed to check the code works for
+// a variety of frequencies.
+// 38400 likely works in all scenarios, YMMV with higher values.  
+
+
+DEFINE_DEBUG_PUTCHAR_FN(PA0, 38400)
 
 int main(void) {
-  // Any of these will work at 57600 baud:
-  CPUCS = 0;
-  // CPUCS = _CLKSPD0;
-  // CPUCS = _CLKSPD1;
-
   OEA = (1U<<0);
   PA0 = 1;
 
-  printf("Hello, world!\n");
+  CPUCS = 0;
+  printf("Hello, world at 12MHz CPU clock!\r\n");
+  
+  CPUCS = _CLKSPD0;
+  printf("Hello, world at 24MHz CPU clock!\r\n");
+  
+  CPUCS = _CLKSPD1;
+  printf("Hello, world at 48MHz CPU clock!\r\n");
+  
   while(1);
 }
