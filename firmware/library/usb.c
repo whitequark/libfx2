3360abe54de7a3741bdcb103b86d172b5a92d36e
Catherine <whitequark@whitequark.org>
2025-10-13 00:39:22 +0000

    Optimize `isr_SUDAV()`.

--- firmware/library/usb.c
+++ firmware/library/usb.c
@@ -68,39 +68,42 @@ __asm
   mov  _DPS, #0
 __endasm;
 
+  uint8_t bmRequestType = req->bmRequestType;
+  uint8_t bRequest = req->bRequest;
+
   // Get Descriptor
-  if(req->bmRequestType == (USB_RECIP_DEVICE|USB_DIR_IN) &&
-     req->bRequest == USB_REQ_GET_DESCRIPTOR) {
+  if(bmRequestType == (USB_RECIP_DEVICE|USB_DIR_IN) &&
+     bRequest == USB_REQ_GET_DESCRIPTOR) {
     enum usb_descriptor type = (enum usb_descriptor)(req->wValue >> 8);
     uint8_t index = req->wValue & 0xff;
     handle_usb_get_descriptor(type, index);
     // Set Configuration
-  } else if(req->bmRequestType == (USB_RECIP_DEVICE|USB_DIR_OUT) &&
-            req->bRequest == USB_REQ_SET_CONFIGURATION) {
+  } else if(bmRequestType == (USB_RECIP_DEVICE|USB_DIR_OUT) &&
+            bRequest == USB_REQ_SET_CONFIGURATION) {
     if(handle_usb_set_configuration((uint8_t)req->wValue)) {
       ACK_EP0();
     } else {
       STALL_EP0();
     }
     // Get Configuration
-  } else if(req->bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_IN) &&
-            req->bRequest == USB_REQ_GET_CONFIGURATION) {
+  } else if(bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_IN) &&
+            bRequest == USB_REQ_GET_CONFIGURATION) {
     handle_usb_get_configuration();
     // Set Interface
-  } else if(req->bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_OUT) &&
-            req->bRequest == USB_REQ_SET_INTERFACE) {
+  } else if(bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_OUT) &&
+            bRequest == USB_REQ_SET_INTERFACE) {
     if(handle_usb_set_interface((uint8_t)req->wIndex, (uint8_t)req->wValue)) {
       ACK_EP0();
     } else {
       STALL_EP0();
     }
     // Get Interface
-  } else if(req->bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_IN) &&
-            req->bRequest == USB_REQ_GET_INTERFACE) {
+  } else if(bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_IN) &&
+            bRequest == USB_REQ_GET_INTERFACE) {
     handle_usb_get_interface((uint8_t)req->wIndex);
     // Set Feature - Device
-  } else if(req->bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_OUT) &&
-            req->bRequest == USB_REQ_SET_FEATURE) {
+  } else if(bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_OUT) &&
+            bRequest == USB_REQ_SET_FEATURE) {
     if(req->wValue == USB_FEAT_DEVICE_REMOTE_WAKEUP) {
       usb_remote_wakeup = true;
       ACK_EP0();
@@ -108,27 +111,27 @@ __endasm;
       ACK_EP0();
     }
     // Get Status - Device
-  } else if(req->bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_IN) &&
-            req->bRequest == USB_REQ_GET_STATUS) {
+  } else if(bmRequestType == (USB_RECIP_DEVICE|USB_TYPE_STANDARD|USB_DIR_IN) &&
+            bRequest == USB_REQ_GET_STATUS) {
     EP0BUF[0] = (usb_self_powered  << 0) |
                 (usb_remote_wakeup << 1);
     EP0BUF[1] = 0;
     SETUP_EP0_IN_BUF(2);
     // Get Status - Interface
-  } else if(req->bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_IN) &&
-            req->bRequest == USB_REQ_GET_STATUS) {
+  } else if(bmRequestType == (USB_RECIP_IFACE|USB_TYPE_STANDARD|USB_DIR_IN) &&
+            bRequest == USB_REQ_GET_STATUS) {
     EP0BUF[0] = 0;
     EP0BUF[1] = 0;
     SETUP_EP0_IN_BUF(2);
     // Set Feature - Endpoint
     // Clear Feature - Endpoint
-  } else if(req->bmRequestType == (USB_RECIP_ENDPT|USB_TYPE_STANDARD|USB_DIR_OUT) &&
-            (req->bRequest == USB_REQ_SET_FEATURE ||
-             req->bRequest == USB_REQ_CLEAR_FEATURE)) {
+  } else if(bmRequestType == (USB_RECIP_ENDPT|USB_TYPE_STANDARD|USB_DIR_OUT) &&
+            (bRequest == USB_REQ_SET_FEATURE ||
+             bRequest == USB_REQ_CLEAR_FEATURE)) {
     if(req->wValue == USB_FEAT_ENDPOINT_HALT) {
       __xdata volatile uint8_t *EPnCS = EPnCS_for_n(req->wIndex);
       if(EPnCS != 0) {
-        if(req->bRequest == USB_REQ_SET_FEATURE) {
+        if(bRequest == USB_REQ_SET_FEATURE) {
           *EPnCS |= _STALL;
           ACK_EP0();
         } else {
@@ -141,8 +144,8 @@ __endasm;
       }
     }
     // Get Status - Endpoint
-  } else if(req->bmRequestType == (USB_RECIP_ENDPT|USB_TYPE_STANDARD|USB_DIR_IN) &&
-            req->bRequest == USB_REQ_GET_STATUS) {
+  } else if(bmRequestType == (USB_RECIP_ENDPT|USB_TYPE_STANDARD|USB_DIR_IN) &&
+            bRequest == USB_REQ_GET_STATUS) {
     __xdata volatile uint8_t *EPnCS = EPnCS_for_n(req->wIndex);
     if(EPnCS != 0) {
       EP0BUF[0] = ((*EPnCS & _STALL) != 0);
